075def5faa1191310705a3fbc760ca04b4aeabb3
Warren Lazarraga <79947453+WarrenLaz@users.noreply.github.com>
2024-04-06 00:23:12 -0400

    1

--- core.py
+++ core.py
@@ -3,7 +3,8 @@ import requests
 
 def GetMovies(j):
     MovieList = []
-    for i in range(j):
+    id = 0
+    for i in range(1,j):
         url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page="+ str(i) +"&sort_by=popularity.desc"
         headers = {
             "accept": "application/json",
@@ -12,23 +13,24 @@ def GetMovies(j):
 
         response = requests.get(url, headers=headers)
         Movies = response.json()["results"]
-        print(Movies)
-        id = 0
         for movie in Movies:
             temp = []
             temp.append(id)
-            temp.append(movie["title"])
+            temp.append(movie["title"].replace(":", ""))
             temp.append(movie["overview"])
             MovieList.append(temp)
+            id += 1
         
         return MovieList
 
 def WritetoText(f, List):
     for item in List:
-            f.write(item[0] + " " + item[1] + " " + item[2] + "\n")
+            f.write(str(item[0]) + "," + item[1] + "," + item[2] + "\n")
     return 0
 
 def main():
     f = open("MovieList.txt", 'w')
     mList = GetMovies(2)
     WritetoText(f, mList)
+
+main()
