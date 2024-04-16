c2fd64d9285de8f605d023d1a985645f0a348d76
Warren Lazarraga <79947453+WarrenLaz@users.noreply.github.com>
2024-04-16 18:52:46 -0400

    new

--- backendtest.py
+++ backendtest.py
@@ -4,9 +4,7 @@ import math
 
 uri = 'mongodb+srv://usern:abcd1234@engine0001.vapavh9.mongodb.net/'
 client = MongoClient(uri, server_api = ServerApi('1'))
-f = open("InvertedIndex.txt", "r")
 swfile = open("stopwords.txt", "r")
-
 #635378
 def pingM():
     try:
@@ -47,14 +45,37 @@ def Freq(list):
             freq[item] = 1
     return freq
 
-def vectorize(d):
-    return 
+def getdocs(query1, dblist1):
+    documents = []
+    
+    for word in query1:
+        t = []
+        wlist = dblist1.find_one({"keyWord" : word}) 
+        t.append(word)
+        t.append(wlist["doc-Freq"])
+        t.append(wlist["mList"])
+        documents.append(t)
+        
+    return documents
+            
+
 
 def main():
+    pingM()
     dblist = getCollection("MovieInvertedIndex", "Iv-Id-m")
-    inp = input("Query: ")
+    inp = input("Query: ").lower()
     query = inp.split()
-    print(Freq(query))
+    ds = getdocs(query, dblist)
+    l = 0
+    for x in ds:
+        print(x[1])
+        l += x[1]
+    for x in ds:
+        print(math.log((1+l)/(x[1]+1)))
+
+
+
+
 
 main()
     
